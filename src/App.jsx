2aa2b85b8648012bb8e4383ea858f2478f38e710
ismael <joseismaelvargas@users.noreply.github.com>
2024-08-27 15:46:22 -0300

    "conceptos de react"

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import'bootstrap/dist/css/bootstrap.min.css'
+import { Info } from './components/Info'
+
+function App() {
+  
+
+  return(
+    <>
+    <main className='container my-4'>
+      <h1 className='text-center'>Hola React como estas bebe</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, deserunt est eaque perferendis praesentium, hic fugit tempora modi iure quia quod qui unde, reiciendis repellat officiis error non velit facilis.</p>
+    
+       <Info></Info>
+    </main>
+
+
+  
+    </>
+  )
+}
+
+export default App
